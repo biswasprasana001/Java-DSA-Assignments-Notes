16add52189ecebfe25c6ad25c31cc1fb6d1afaa4
Biswas <biswasprasana004@gmail.com>
2021-11-08 14:05:57 +0530

    calculator program modified

--- 1. First Java Program/4. Calculator/src/com/company/Main.java
+++ 1. First Java Program/4. Calculator/src/com/company/Main.java
@@ -6,36 +6,41 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a Number: ");
-        double num1 = input.nextDouble();
-        System.out.print("Enter another Number: ");
-        double num2 = input.nextDouble();
-        System.out.print("Enter an Operator: ");
-        String op = input.next();
-        if (op == "+") {
-            double sum = num1 + num2;
-            System.out.println("Sum is " + sum);
-        }
-        else if (op == "-") {
-            double diff;
-            if(num1 >= num2) {
-                diff = num1 - num2;
+        while (true) {
+            System.out.print("Enter an Operator: ");
+            char op = input.next().trim().charAt(0);
+            if (op == 'x'){
+                System.out.println("Program Stopped");
+                break;
+            }
+            System.out.print("Enter a Two Numbers: ");
+            double num1 = input.nextDouble();
+            double num2 = input.nextDouble();
+            if (op == '+') {
+                double sum = num1 + num2;
+                System.out.println("Sum is " + sum);
+            } else if (op == '-') {
+                double diff;
+                if (num1 >= num2) {
+                    diff = num1 - num2;
+                } else {
+                    diff = num2 - num1;
+                }
+                System.out.println("Difference is " + diff);
+            } else if (op == '*') {
+                double pro = num1 * num2;
+                System.out.println("The product is " + pro);
+            } else if (op == '/') {
+                if (num2 != 0) {
+                    double quo = num1 / num2;
+                    System.out.println("Quotient is " + quo);
+                } else {
+                    System.out.println("Enter A Valid Second Number");
+                }
             }
             else {
-                diff = num2 - num1;
+                System.out.println("Enter a Valid Operator");
             }
-            System.out.println("Difference is " + diff);
         }
-        else if (op == "*") {
-            double pro = num1 * num2;
-            System.out.println("The product is " + pro);
-        }
-        else if (op == "/") {
-            double quo = num1/num2;
-            System.out.println("Quotient is" + quo);
-        }
-//        else {
-//            System.out.println("Enter a Valid Operator");
-//        }
     }
 }
