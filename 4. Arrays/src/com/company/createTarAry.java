2f67bf232fcf079f6a12bcb87ac3f1ee2197c4b1
Biswas <biswasprasana004@gmail.com>
2021-11-17 11:51:34 +0530

    finally solved the target questions

--- 4. Arrays/src/com/company/createTarAry.java
+++ 4. Arrays/src/com/company/createTarAry.java
@@ -1,12 +1,28 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class createTarAry {
     public static void main(String[] args) {
         int[] nums = {0, 1, 2, 3, 4};
+        int[] index = {0, 1, 2, 2, 1};
+        System.out.println(Arrays.toString(createTargetArray(nums, index)));
+
     }
 
     static int[] createTargetArray(int[] nums, int[] index) {
-
-        return nums;
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            a.add(index[i],nums[i]);
+        }
+        int target[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            target[i] = a.get(i);
+        }
+        return target;
     }
 }
