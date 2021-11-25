1e783928a72e52b5d69e6682569013b98b6fbe25
biswasprasana001 <biswasprasana004@gmail.com>
2021-11-25 22:48:22 +0530

    solved the hectic question

--- 4. Arrays/src/com/company/maximumPopulation.java
+++ 4. Arrays/src/com/company/maximumPopulation.java
@@ -3,15 +3,23 @@ package com.company;
 public class maximumPopulation {
     public static void main(String[] args) {
         int[][] logs = {{1993, 1999},{2000, 2010}};
-        maximumPopulation(logs);
+        System.out.println(maximumPopulation(logs));
     }
     static int maximumPopulation(int[][] logs) {
-        int count = 0;
-        for (int i = 0; i < logs.length - 1; i++) {
-            if (logs[i][1] == logs[i+1][0]) {
-                count += 2;
+        int[] arr = new int[101];
+        for (int i = 0; i < logs.length; i++) {
+            for (int j = logs[i][0]; j < logs[i][1]; j++) {
+                arr[j-1950]++;
             }
         }
-        return 0;
+        int maxValue = 0;
+        int firstYear = 1950;
+        for (int i = 0; i < arr.length; i++) {
+            if(maxValue < arr[i]) {
+                maxValue = arr[i];
+                firstYear = i + 1950;
+            }
+        }
+        return firstYear;
     }
 }
