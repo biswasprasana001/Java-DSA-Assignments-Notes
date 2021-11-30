c26cd3528facdaecf5d8a171b3b7253c308542f5
biswasprasana001 <biswasprasana004@gmail.com>
2021-12-01 00:54:30 +0530

    will be solving the final problem

--- 4. Arrays/src/com/company/plusOne.java
+++ 4. Arrays/src/com/company/plusOne.java
@@ -1,13 +1,28 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class plusOne {
     public static void main(String[] args) {
-        int[] digits = {1, 1, 9};
-        plusOne(digits);
+        int[] digits = {9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
     }
-    static ArrayList<Integer> plusOne(int[] digits) {
-        return null;
+    static int[] plusOne(int[] digits) {
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                break;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        if(digits[0] == 0) {
+            int[] newDigits = new int[n + 1];
+            newDigits[0] = 1;
+            return newDigits;
+        }
+        return digits;
     }
 }
---
+++ 4. Arrays/src/com/company/removeDuplicates.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class removeDuplicates {
+    public static void main(String[] args) {
+
+    }
+}
