59e55ad3315ad8979a7888a17ed7d7bab3359b03
biswasprasana001 <biswasprasana004@gmail.com>
2022-01-04 01:06:38 +0530

    Understood With the Help of Mechanism I Discovered

---
+++ 5. Searching/src/com/company/findPivot.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class findPivot {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int out = searchPivot(arr);
+        System.out.println(out);
+    }
+    static int searchPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            // 4 cases over here
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                return mid-1;
+            }
+            if (arr[mid] < arr[start]) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
